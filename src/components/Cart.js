e3ca212a641774e7d0b4ecad7b83599540855231
Alanna Wellenkamp <actuallyalanna@gmail.com>
2020-11-15 10:30:02 -0800

    added individual component files

---
+++ src/components/Cart.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import CartItem from './CartItem';
+import CartTotal from './CartTotal';
+
+class Cart extends Component {
+
+    render() {
+        const summary = Object.keys(this.state.selected).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const selectedOption = this.state.selected[feature];
+
+            return (
+                <CartItem
+                    feature={feature}
+                    featureHash={featureHash}
+                    selectedOption={selectedOption}
+                    key={selectedOption.name}
+                    cost={selectedOption.cost}
+                />
+            );
+        });
+
+        return (
+            <section className='main_summary'>
+                <h2>Your Cart</h2>
+                {summary}
+                <CartTotal selected={this.props.selected} />
+            </section>
+        )
+    }
+}
+
+export default Cart
---
+++ src/components/OptionsList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import OptionItem from './OptionItem';
+import OptionFeature from './OptionFeature';
+import slugify from 'slugify';
+
+class OptionList extends Component {
+    render() {
+        const features = Object.keys(this.props.features).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const options = this.props.features[feature].map(item => {
+                const itemHash = slugify(JSON.stringify(item));
+                return (
+                    <OptionItem
+                        selected={this.props.selected}
+                        itemHash={itemHash}
+                        feature={feature}
+                        item={item}
+                        key={item.name}
+                        updateFeature={this.props.updateFeature}
+                    />
+                );
+            });
+
+            return (
+                <OptionFeature
+                    key={idx}
+                    feature={feature}
+                    options={options}
+                    featureHash={featureHash}
+                />
+
+            );
+        });
+
+        return (
+            <form className="main__form">
+                <h2>Customize your laptop</h2>
+                {features}
+            </form>
+        )
+    }
+
+}
+export default OptionList
